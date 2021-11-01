8e2e4b17b4fa6b24ac06e671ce22819fd8bf8c51
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2021-11-01 11:59:57 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const flagDictionary = {
+  "🏁": "Chequered Flag",
+  "🚩": "Triangular Flag",
+  "🎌": "Crossed Flags",
+  "🏴": "Black Flag",
+  "🏳️": "White Flag",
+  "🏳️‍🌈": "Rainbow Flag",
+  "🏳️‍⚧️": "Transgender Flag",
+  "🏴‍☠️": "Pirate Flag"
+};
+
+var flags = Object.keys(flagDictionary);
+export default function App() {
+  const [meaning, setmeaning] = useState("");
+  function flagInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = flagDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "Sorry this flag is not in our database!";
+    }
+    setmeaning(userInput + meaning);
+  }
+
+  function flagClickHandler(flag) {
+    setmeaning(flag + flagDictionary[flag]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Flag Interpretor</h1>
+      <input onChange={flagInputHandler} />
+      <h2>{meaning}</h2>
+      <h3>Flags we know</h3>
+      {flags.map(function (flag) {
+        return (
+          <span
+            onClick={() => flagClickHandler(flag)}
+            style={{ fontSize: "2rem", padding: "0.5rem" }}
+            key={flag}
+          >
+            {flag}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
